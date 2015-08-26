7c76ed2b371e5537fc20c3f8aed5f28cc2221492
PJ Davis <pj.davis@gmail.com>
2015-08-26 15:08:55 -0500

    Failing spec for cases of divisible by 3 and 5 ('fizzbuzz' not implemented yet)

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -7,7 +7,13 @@ class FizzBuzz
   def generate
     fizz_buzz = []
     1.upto(@upto) do |i|
-      fizz_buzz << i
+      if i % 3 == 0
+        fizz_buzz << 'fizz'
+      elsif i % 5 == 0
+        fizz_buzz << 'buzz'
+      else
+        fizz_buzz << i
+      end
     end
     fizz_buzz
   end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,7 +2,21 @@ require 'fizzbuzz'
 
 RSpec.describe FizzBuzz, '#generate' do
   it "generates numbers up to the end" do
-    fizzbuzz = FizzBuzz.new(20)
-    expect(fizzbuzz.generate.size).to eq(20)
+    fizz_buzz = FizzBuzz.new(20)
+    expect(fizz_buzz.generate.size).to eq(20)
+  end
+
+  it "replaces numbers divisable by 3 with 'fizz'" do
+    fizz_buzz = FizzBuzz.new(20)
+    (1..20).select {|i| i % 3 == 0}.each do |i|
+      expect(fizz_buzz.generate[i - 1]).to eq('fizz')
+    end
+  end
+
+  it "replaces numbers divisable by 5 with 'buzz'" do
+    fizz_buzz = FizzBuzz.new(100)
+    (1..100).select {|i| i % 5 == 0}.each do |i|
+      expect(fizz_buzz.generate[i - 1]).to eq('buzz')
+    end
   end
 end
